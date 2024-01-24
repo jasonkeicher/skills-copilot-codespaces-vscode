be6781280204ad9e9665d35af8f208d9fa444051
Jason Keicher <3915176+jasonkeicher@users.noreply.github.com>
2024-01-24 16:38:28 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 30,
+        skills: ["js", "html", "css"],
+        salary: 1000
+    }
+    return member;
+}
+
